8884eb6ed292d85f04ce20693470a32a96936ba6
renchao <542607047@qq.com>
2024-08-16 07:40:32 +0800

    更新debug模块依赖

--- spring-boot-project/spring-boot-debug/src/main/java/com/renchao/boot/Demo01.java
+++ spring-boot-project/spring-boot-debug/src/main/java/com/renchao/boot/Demo01.java
@@ -4,6 +4,8 @@ import com.renchao.boot.service.MyService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author ren_chao
@@ -15,5 +17,14 @@ public class Demo01 {
 		ConfigurableApplicationContext context = SpringApplication.run(Demo01.class, args);
 		MyService bean = context.getBean(MyService.class);
 		System.out.println("=================::" + bean.getName());
+	}
+
+	@RestController
+	static class MyController {
+
+		@RequestMapping("/test")
+		public String test() {
+			return "hello world";
+		}
 	}
 }
